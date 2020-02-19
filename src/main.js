6650a5a48d999b2c9172de344ade84caad9a9f70
katsuyukishimbo <katsuyuki0611@gmail.com>
2020-02-19 23:55:48 +0900

    install typescript

--- src/main.js
+++ src/main.js
@@ -3,6 +3,16 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+Vue.config.ignoredElements = [
+  "mwc-drawer",
+  "mwc-top-app-bar",
+  "mwc-button",
+  "mwc-icon-button",
+  "mwc-dialog",
+  "mwc-textfield"
+];
+
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
