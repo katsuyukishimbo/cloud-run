3d110177f7bc319e02cb4c3327d8e3a504447517
katsuyukishimbo <katsuyuki0611@gmail.com>
2020-02-21 22:03:02 +0900

    add route

---
+++ src/views/About.vue
@@ -0,0 +1,31 @@
+<template>
+  <div slot="appContent">
+    <mwc-top-app-bar>
+      <mwc-icon-button
+        slot="navigationIcon"
+        icon="menu"
+        v-on:click="toggleDrawer"
+      ></mwc-icon-button>
+      <div slot="title">About</div>
+    </mwc-top-app-bar>
+    <div class="main-content">
+      <p>
+        Sample WebSite for serverless admin screen.
+      </p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import "@material/mwc-top-app-bar";
+import "@material/mwc-icon-button";
+import AppComponent from '../App.vue';
+@Component
+export default class AboutPage extends Vue {
+  toggleDrawer(e: Event) {
+    const parent = this.$parent as AppComponent;
+    parent.openDrawer = !parent.openDrawer;
+  }
+}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div slot="appContent">
+    <mwc-top-app-bar>
+      <mwc-icon-button
+        slot="navigationIcon"
+        icon="menu"
+        v-on:click="toggleDrawer"
+      ></mwc-icon-button>
+      <div slot="title">Home</div>
+    </mwc-top-app-bar>
+    <div class="main-content">
+      <img alt="Vue logo" src="../assets/logo.png" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import "@material/mwc-top-app-bar";
+import "@material/mwc-icon-button";
+import AppComponent from '../App.vue';
+@Component
+export default class HomeComponent extends Vue {
+  toggleDrawer(e: Event) {
+    const parent = this.$parent as AppComponent;
+    parent.openDrawer = !parent.openDrawer;
+  }
+}
+</script>
